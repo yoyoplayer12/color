dee623f0d408ac70e3e6edd09ddbddf618601921
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-18 15:42:05 +0100

    test

--- controllers/api/v1/colors.js
+++ controllers/api/v1/colors.js
@@ -3,32 +3,6 @@ const Color = require("../../../models/Color");
 
 const index = async (req, res) => {
     let colors;
-    if(req.query.color1) {
-        try{
-            let colors = await Color.find({ color1: req.query.color1 });
-            if (colors.length > 0) {
-                res.json({
-                    "status": "success",
-                    "message": "GETTING colorarray for color" + req.query.color1,
-                    "data": {
-                        "messages": colors
-                    }
-                });
-            } else {
-                res.json({
-                    "status": "error",
-                    "message": "No colorarray found for color " + req.query.color1
-                });
-            }
-        } catch (err) {
-            console.error(err);
-            res.json({
-                "status": "error",
-                "message": "An error occurred while getting the colorarray."
-            });
-        }
-    }
-    else {
         try{
             colors = await Color.find({});
             res.json({
@@ -47,8 +21,7 @@ const index = async (req, res) => {
             });
         }
     }
-    
-};
+
 
 const create = async(req, res) => {
     let m = new Color();
