ff6430d75829601d76b492fb6e6535ca22ece795
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-18 15:09:29 +0100

    fixing

--- controllers/api/v1/colors.js
+++ controllers/api/v1/colors.js
@@ -2,36 +2,121 @@
 const Color = require("../../../models/Color");
 
 const index = async (req, res) => {
-    let colors = [req.query.color1, req.query.color2, req.query.color3, req.query.color4, req.query.color5];
-    let results = [];
-    for (let i = 0; i < colors.length; i++) {
-        if (colors[i]) {
-            try {
-                let color = await Color.find({ ['color' + (i + 1)]: colors[i] });
-                if (color.length > 0) {
-                    results.push({
-                        "status": "success",
-                        "message": "GETTING colors for color" + (i + 1) + " " + colors[i],
-                        "data": {
-                            "color": color
-                        }
-                    });
-                } else {
-                    results.push({
-                        "status": "error",
-                        "message": "No color found for color " + colors[i]
-                    });
-                }
-            } catch (err) {
-                console.error(err);
+    let color1 = req.query.color1;
+    let color2 = req.query.color2;
+    let color3 = req.query.color3;
+    let color4 = req.query.color4;
+    let color5 = req.query.color5;
+    let colors = [];
+    if(req.query.color1) {
+        try {
+            let color1 = await Color.find({ color1: req.query.color1 });
+            let color2 = await Color.find({ color2: req.query.color2 });
+            let color3 = await Color.find({ color3: req.query.color3 });
+            let color4 = await Color.find({ color4: req.query.color4 });
+            let color5 = await Color.find({ color5: req.query.color5 });
+            if (color1.length > 0) {
+                res.json({
+                    "status": "success",
+                    "message": "GETTING colors for color1 " + req.query.color1,
+                    "data": {
+                        "color": color1
+                    }
+                });
+            } else {
                 res.json({
                     "status": "error",
-                    "message": "An error occurred while getting the colors."
+                    "message": "No color found for color " + req.query.color1
                 });
-                return;
             }
+
+            if (color2.length > 0) {
+                res.json({
+                    "status": "success",
+                    "message": "GETTING colors for color2 " + req.query.color2,
+                    "data": {
+                        "color": color2
+                    }
+                });
+            } else {
+                res.json({
+                    "status": "error",
+                    "message": "No color found for color " + req.query.color2
+                });
+            }
+
+            if (color3.length > 0) {
+                res.json({
+                    "status": "success",
+                    "message": "GETTING colors for color3 " + req.query.color3,
+                    "data": {
+                        "color": color3
+                    }
+                });
+            } else {
+                res.json({
+                    "status": "error",
+                    "message": "No color found for color " + req.query.color3
+                });
+            }
+
+            if (color4.length > 0) {
+                res.json({
+                    "status": "success",
+                    "message": "GETTING colors for color4 " + req.query.color4,
+                    "data": {
+                        "color": color4
+                    }
+                });
+            } else {
+                res.json({
+                    "status": "error",
+                    "message": "No color found for color " + req.query.color4
+                });
+            }
+
+            if (color5.length > 0) {
+                res.json({
+                    "status": "success",
+                    "message": "GETTING colors for color4 " + req.query.color5,
+                    "data": {
+                        "color": color5
+                    }
+                });
+            } else {
+                res.json({
+                    "status": "error",
+                    "message": "No color found for color " + req.query.color5
+                });
+            }
+        } catch (err) {
+            console.error(err);
+            res.json({
+                "status": "error",
+                "message": "An error occurred while getting the colors."
+            });
         }
     }
+    else {
+        try{
+            colors = await Color.find({});
+            res.json({
+                status: "success",
+                message: "GETTING colors",
+                data: [{
+                    messages: color1,color2,color3,color4,color5
+                }],
+            });
+        }
+        catch(err){
+            console.error(err);
+            res.json({
+                status: "error",
+                message: "An error occurred while getting the colors.",
+            });
+        }
+    }
+    
 };
 
 const create = async(req, res) => {
